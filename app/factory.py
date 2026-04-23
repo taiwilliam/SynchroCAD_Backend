6aaddc4d1498fcd4c09291cbc248501ea7d724fb
William Tai <44943338+taiwilliam@users.noreply.github.com>
2026-04-23 14:11:43 +0800

    create swagger

--- app/factory.py
+++ app/factory.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flasgger import Swagger
 from app.extensions import db, migrate
 from app.routers import main_bp, users_bp
 from app.models import *
@@ -7,6 +8,19 @@ from app.cli import register_cli_commands
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.settings.Config')
+
+    swagger_template = {
+        "swagger": "2.0",
+        "info": {
+            "title": "SynchroCAD Backend API",
+            "description": "REST API documentation",
+            "version": "1.0.0",
+        },
+        "basePath": "/",
+        "schemes": ["http", "https"],
+    }
+    Swagger(app, template=swagger_template)
+
     db.init_app(app)
     migrate.init_app(app, db)
     app.register_blueprint(main_bp)
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,36 +1,45 @@
 """
 User Router - RESTful API routes for users.
 """
+import os
 from flask import Blueprint
+from flasgger import swag_from
 from app.controllers import UserController
 from app.http import json_body
 
 # 建立 blueprint
 users_bp = Blueprint('users', __name__, url_prefix='/api/users')
 
+_docs = os.path.join(os.path.dirname(__file__), '..', 'docs', 'users')
+
 
 @users_bp.route('', methods=['GET'])
+@swag_from(os.path.join(_docs, 'index.yml'))
 def index():
     return UserController.list()
 
 
 @users_bp.route('/<int:user_id>', methods=['GET'])
+@swag_from(os.path.join(_docs, 'show.yml'))
 def show(user_id):
     return UserController.get(user_id)
 
 
 @users_bp.route('', methods=['POST'])
+@swag_from(os.path.join(_docs, 'store.yml'))
 def store():
     data = json_body()
     return UserController.create(data)
 
 
 @users_bp.route('/<int:user_id>', methods=['PUT'])
+@swag_from(os.path.join(_docs, 'update.yml'))
 def update(user_id):
     data = json_body()
     return UserController.update(user_id, data)
 
 
 @users_bp.route('/<int:user_id>', methods=['DELETE'])
+@swag_from(os.path.join(_docs, 'destroy.yml'))
 def destroy(user_id):
     return UserController.delete(user_id)
